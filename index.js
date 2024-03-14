4774f0867d386aff39c5a980349dd5696535b7f1
Emanuele <emanuele.pez1@gmail.com>
2024-03-14 16:43:15 +0100

    added v0.5  js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const generateTable = () => {
   for (let i = 0; i < 8; i++) {
     const row = document.createElement('tr')
     for (let j = 0; j < 10; j++) {
-      if (number <= 76) {
+      if (number <= 75) {
         const cell = document.createElement('td')
         cell.innerText = number
         row.appendChild(cell)
@@ -21,3 +21,21 @@ const generateTable = () => {
 }
 
 generateTable()
+
+const bingoNumbers = []
+for (let i = 1; i <= 75; i++) {
+  bingoNumbers.push[i]
+}
+const drawnNumbers = []
+
+const drawNumber = () => {
+  if (bingoNumbers === 0) {
+    return 'All numbers have been drawn!'
+  }
+
+  const randomNumber = Math.floor(Math.random() * bingoNumbers.length)
+  const number = bingoNumbers.splice(randomNumber, 1)[0]
+  drawnNumbers.push(number)
+  return number
+}
+console.log(drawNumber())
